31e41cf9ea3dcc7a489bec57387301a265830ff4
yagpdc <ysantanaf@gmail.com>
2024-05-04 13:51:24 -0300

    novos conteudos

---
+++ 2023/ps_abrindoportas/models/user.models.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(id, nome, email, idade) {
+      this.id = id;
+      this.nome = nome;
+      this.email = email;
+      this.idade = idade;
+    }
+  }
+  
+  module.exports = User;
